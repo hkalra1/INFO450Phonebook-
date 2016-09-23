8c6e8ffe9ecb8969c6dab22cef009f0ccbf6d58c
Harry Kalra <kalra_harry@hotmail.com>
2016-09-22 22:31:14 -0400

    initial

--- phoneBook2/phoneBook2.cpp
+++ phoneBook2/phoneBook2.cpp
@@ -2,10 +2,42 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+char phoneBook[10][2][50];
+void addName(char mylist[10][2][50]);
+void showList(char mylist[10][2][50]);
+void confirm(char mylist[10][2][50]);
+
 
 
 int main()
 {
-    return 0;
+	char mylist[10][2][50];
+	int i = 0;
+	char answer;
+	do
+	{
+		cout << "-----------------------" << endl;
+		cout << "What do you want to do?" << endl;
+		cout << "-----------------------" << endl;
+		cout << "[A]dd new entry -- enter A" << endl;
+		cout << "[D]isplay -- enter D" << endl;
+		cout << "[Q]uit -- enter Q" << endl;
+		cin >> answer;
+	}
 }
 
+void addName(char mylist[10][2][50])
+{
+}
+
+void showList(char mylist[10][2][50])
+{
+}
+
+void confirm(char mylist[10][2][50])
+{
+}
